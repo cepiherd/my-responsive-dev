f09e815a1bfd7d370eb7a382278d02d074c8b7f4
ngab_cepi <cepi@niagateknologi.net>
2022-11-03 14:15:19 +0700

    :hammer: updated design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	let link = 'https://i-sedekah.id';
+	let link = '';
 
 	let screenMotive = [
 		{
@@ -7,13 +7,18 @@
 			width: 428,
 			height: 928
 		},
-        {
+		{
 			title: 'Samsung',
 			width: 360,
 			height: 800
 		},
 		{
-			title: 'Macbook Pro',
+			title: 'Ipad Mini',
+			width: 768,
+			height: 1024
+		},
+		{
+			title: 'Macbook Air',
 			width: 1280,
 			height: 800
 		}
@@ -25,22 +30,30 @@
 </script>
 
 <svelte:window bind:innerWidth={x} bind:innerHeight={y} />
-<section >
-    <h1 class="text-white">My Responsive dev</h1>
-	<input type="text" bind:value={link} class="min-w-[400px] rounded-md py-3 px-8 m-2" />
-	<div class="flex gap-1 relative  ">
+<section>
+    <h1 class="font-extrabold text-white text-center py-10 text-2xl">Enjoy localhost develop</h1>
+	<input
+		type="text"
+		bind:value={link}
+        placeholder="Your url"
+		class="min-w-[400px] rounded-md py-3 px-8 m-2 bg-white/10 shadow-lg text-white"
+	/>
+	<div class={`flex gap-4 relative scale-[.6] absolute -mt-[10em] -ml-[22rem]`}>
 		{#each screenMotive as screen}
-			<div class="text-white w-full scale-[0.6] max-w-[300px] max-h-0 relative ">
-				<p>{screen.title}</p> 
-                <p>{screen.width} x {screen.height}</p>
+			<div
+				class={`text-white  relative `}
+				style={`max-width:${screen.width}; max-height:${screen.height}`}
+			>
+				<p class="bg-red-400 rounded text-center py-3">{screen.title}</p>
+				<p>{screen.width} x {screen.height}</p>
 				<iframe
+					sandbox="allow-top-navigation allow-scripts allow-forms"
 					src={link}
 					width={screen.width}
 					height={screen.height}
 					style={`border:1px solid black;`}
 					title={screen.title}
-                    class="-ml-10"
-                    
+					class=" bg-white  rounded-lg"
 				/>
 			</div>
 		{/each}
